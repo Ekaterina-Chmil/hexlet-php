498a3d93b58541e07d38d7c2bf39279e9904b9b5
Екатерина Чмиль <chmil.ekat@yandex.ru>
2024-12-01 17:01:52 +0700

    Add PHP_CodeSniffer and fix code style issues

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-echo 'Hello, Hexlet!';
-?>
+/**
+ * Краткое описание функциональности файла.
+ *
+ * @category ExampleCategory
+ * @package  ExamplePackage
+ * @author   Ekaterina Chmil <your-email@example.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @version  GIT: abc12345
+ * @link     https://example.com
+ * @php      8.4.1
+ */
+
+declare(strict_types=1);
+
+// Ваш основной код начинается здесь.
+
+require_once __DIR__ . '/vendor/autoload.php';
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
+
